681690889c3a4dd8447b6c1ec11ea0fc357dbed1
Adrien KISSIE <fredkiss3@gmail.com>
2023-04-17 21:49:15 +0200

    💄 remove lg styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import { TailwindIndicator } from "./_components/tailwind-indicator";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,19 +21,19 @@ export default function RootLayout({
       <head></head>
       <body suppressHydrationWarning className="dark">
         <main className="flex min-h-screen flex-col items-center justify-between p-24 h-screen">
-          <div className="z-10 w-full h-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+          <div className="z-10 w-full h-full max-w-5xl items-center justify-between font-mono text-sm">
             <Link
               href={`/`}
-              className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+              className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit"
             >
               Go home
             </Link>
 
             {children}
 
-            <footer className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+            <footer className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black">
               <a
-                className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                className="pointer-events-none flex place-items-center gap-2 p-8"
                 href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -50,6 +51,7 @@ export default function RootLayout({
             </footer>
           </div>
         </main>
+        <TailwindIndicator />
       </body>
     </html>
   );
