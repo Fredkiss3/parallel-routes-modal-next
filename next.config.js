f84084b53b519a90c2e4a4bb7d0f56e6b7f5d253
Adrien KISSIE <fredkiss3@gmail.com>
2023-06-20 09:38:31 +0200

    ⬆️ upgrade next to canary

--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    deviceSizes: [320, 384, 420, 640, 768, 1080],
-    imageSizes: [64, 128, 256],
-    minimumCacheTTL: 31 * 24 * 60 * 60, // 31 days
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-      {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-      },
-    ],
-  },
+    images: {
+        deviceSizes: [320, 384, 420, 640, 768, 1080],
+        imageSizes: [64, 128, 256],
+        minimumCacheTTL: 31 * 24 * 60 * 60, // 31 days
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "res.cloudinary.com",
+            },
+            {
+                protocol: "https",
+                hostname: "raw.githubusercontent.com",
+            },
+        ],
+    },
 };
 
 module.exports = nextConfig;
